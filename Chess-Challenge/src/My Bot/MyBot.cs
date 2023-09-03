41c4b83a8c571378219698dd7f2288470521e6f7
trangml <mattluutrang@vt.edu>
2023-09-03 11:52:04 -0400

    Very stupid bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,35 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        foreach (Move move in moves)
+        {
+            if (move.IsCapture)
+            {
+                return move;
+            }
+            else if (move.IsPromotion)
+            {
+                return move;
+            }
+            else if (move.IsCastles)
+            {
+                return move;
+            }
+            else if (move.IsEnPassant)
+            {
+                return move;
+            }
+        }
+
+        // Pick a random move to play if nothing better is found
+        Random rng = new();
+        Move moveToPlay = moves[rng.Next(moves.Length)];
+        return moveToPlay;
+
     }
 }
